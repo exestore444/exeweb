699a5242179254261eda1ed85b6111efa2a478dc
exestoree <exewebgupta@gmail.com>
2026-02-26 00:36:18 -0800

    Create HelloServlet.java

---
+++ HelloServlet.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Java Servlet Working 🚀</h1>");
+    }
+}
